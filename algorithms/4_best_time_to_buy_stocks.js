0049115306fafbfc7718f57c0dfc59274a42a2d7
ArthurPopko <artur.popko@gmail.com>
2023-10-29 01:37:14 +0200

    max Profit

--- algorithms/4_best_time_to_buy_stocks.js
+++ algorithms/4_best_time_to_buy_stocks.js
@@ -36,4 +36,16 @@ var maxProfit = function (prices) {
 	return maxProfit
 }
 
+var maxProfitLessCode = function (prices) {
+	let left = 0,
+		maxProfit = 0
+
+	for (let right = 1; right < prices.length; right++) {
+		if (prices[left] > prices[right]) left = right
+		if (prices[right] - prices[left] > maxProfit)
+			maxProfit = prices[right] - prices[left]
+	}
+	return maxProfit
+}
+
 console.log(maxProfit(prices_1))
