89b2691ba443489199aac10341fdbc2cd1b564fa
apopko <apopko@roadie.com>
2023-10-28 20:46:40 +0200

    best time to buy stocks

---
+++ algorithms/4_best_time_to_buy_stocks.js
@@ -0,0 +1,40 @@
+/*
+You are given an array prices where prices[i] 
+is the price of a given stock on the i-th day.
+
+You want to maximize your profit by choosing a single day to buy one stock 
+and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. 
+If you cannot achieve any profit, return 0.
+
+Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy 
+before you sell.
+*/
+//! O(n):
+let prices_1 = [7, 1, 5, 3, 6, 4]
+//				   ^  ^
+
+var maxProfit = function (prices) {
+	let left = 0,
+		right = 1,
+		maxProfit = 0
+
+	while (right < prices.length) {
+		if (prices[left] > prices[right]) {
+			left = right
+		}
+		if (prices[right] - prices[left] > maxProfit) {
+			console.log(maxProfit)
+			maxProfit = prices[right] - prices[left]
+		}
+		right++
+	}
+	return maxProfit
+}
+console.log(maxProfit(prices_1))
