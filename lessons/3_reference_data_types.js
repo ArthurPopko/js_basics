01e55424a92e96b53917623063d97e7708fb3c7b
ArthurPopko <artur.popko@gmail.com>
2023-11-17 21:46:40 +0100

    reference data types upd

--- lessons/3_reference_data_types.js
+++ lessons/3_reference_data_types.js
@@ -7,9 +7,16 @@
 let person = {
 	birth_year: 1982,
 }
+let arr = [1, 2]
+let arr1 = arr
+
+arr1.push(0)
 
 function yourAgeIs(person) {
 	let age = new Date().getFullYear() - person.birth_year
 	console.log(age)
 }
 yourAgeIs(person)
+
+console.log(arr)
+console.log(arr1)
