2b42917358fc0611e7da73f0331ae1dccadc216a
ArthurPopko <artur.popko@gmail.com>
2024-11-22 08:04:35 +0100

    123

--- algorithms/0_squared_array.js
+++ algorithms/0_squared_array.js
@@ -21,4 +21,24 @@ function sqArr(arr) {
 	}
 	return newArr
 }
-console.log(sqArr(arr))
+// console.log(sqArr(arr))
+
+function sqArr_1(arr) {
+	let r = []
+	arr.forEach(num => {
+		console.log(123)
+		 r.push(num**2)
+	})
+	return r.sort()
+}
+
+function sqArr_2(arr) {
+    let r = [];
+    for (let i = 0; i < arr.length; i++) {
+        r.push(arr[i] ** 2); 
+    }
+    r.sort((a, b) => a - b);
+    return r;
+}
+
+console.log(sqArr_2(['as', -1, 0, 3+5, '10']));
